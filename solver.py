84b8bd7b43999732c2f7d69d53e2d56c8c1b0efa
rudelittlegoblin27 <jahagirdar.ramabai@gmail.com>
2026-08-13 08:41:11 +0530

    Add move_D and move_D_prime functions

--- solver.py
+++ solver.py
@@ -1,3 +1,5 @@
+# solver.py
+
 def solved_cube():
     """Returns a fresh, solved cube. Each face is a list of 9 stickers."""
     return {
@@ -24,20 +26,25 @@ def print_cube(cube):
     for i in range(0, 9, 3):
         print('      ' + row('D', i))
 
+
 def rotate_face_cw(face):
     """Rotates a single face's 9 stickers 90 degrees clockwise."""
     a, b, c, d, e, f, g, h, i = face
     return [g, d, a, h, e, b, i, f, c]
 
 
+def rotate_face_ccw(face):
+    """Rotates a single face's 9 stickers 90 degrees counter-clockwise."""
+    a, b, c, d, e, f, g, h, i = face
+    return [c, f, i, b, e, h, a, d, g]
+
+
 def move_U(cube):
     """Applies a clockwise turn of the Up face."""
     cube['U'] = rotate_face_cw(cube['U'])
 
-    # Save F's top row before overwriting anything
     f_top = cube['F'][0:3]
 
-    # Shift top rows around: L -> F -> R -> B -> L
     cube['F'][0:3] = cube['R'][0:3]
     cube['R'][0:3] = cube['B'][0:3]
     cube['B'][0:3] = cube['L'][0:3]
@@ -45,6 +52,56 @@ def move_U(cube):
 
     return cube
 
+
+def move_U_prime(cube):
+    """Applies a counter-clockwise turn of the Up face (undoes move_U)."""
+    cube['U'] = rotate_face_ccw(cube['U'])
+
+    f_top = cube['F'][0:3]
+
+    cube['F'][0:3] = cube['L'][0:3]
+    cube['L'][0:3] = cube['B'][0:3]
+    cube['B'][0:3] = cube['R'][0:3]
+    cube['R'][0:3] = f_top
+
+    return cube
+
+
+def move_D(cube):
+    """Applies a clockwise turn of the Down face."""
+    cube['D'] = rotate_face_cw(cube['D'])
+
+    f_bottom = cube['F'][6:9]
+
+    cube['F'][6:9] = cube['L'][6:9]
+    cube['L'][6:9] = cube['B'][6:9]
+    cube['B'][6:9] = cube['R'][6:9]
+    cube['R'][6:9] = f_bottom
+
+    return cube
+
+
+def move_D_prime(cube):
+    """Applies a counter-clockwise turn of the Down face (undoes move_D)."""
+    cube['D'] = rotate_face_ccw(cube['D'])
+
+    f_bottom = cube['F'][6:9]
+
+    cube['F'][6:9] = cube['R'][6:9]
+    cube['R'][6:9] = cube['B'][6:9]
+    cube['B'][6:9] = cube['L'][6:9]
+    cube['L'][6:9] = f_bottom
+
+    return cube
+
+
 if __name__ == '__main__':
     cube = solved_cube()
+    print("Solved:")
+    print_cube(cube)
+
+    move_D(cube)
+    move_D_prime(cube)
+
+    print("\nAfter D then D' (should match the solved cube above):")
     print_cube(cube)
